66fde503f365946c5480d01d990d388deecd0984
yuanweizhong <yuanweizhong@sensetime.com>
2025-07-15 10:33:04 +0800

    init

---
+++ pda_process.sh
@@ -0,0 +1,6 @@
+# 1.运行lidar2image.py 
+# root_path = '/data/senseauto/高速远距离数据/2025_01_10_10_03_03_AutoCollect_pilotGtRawParser/'
+# 设置为入参
+# 2.运行knn_insert.py
+# root_path = '/data/senseauto/高速远距离数据/2025_01_10_10_03_03_AutoCollect_pilotGtRawParser/'
+# 设置为入参
